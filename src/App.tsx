597a4a4ed136971d682faa39f815d3dc6e211747
Patrick Barringer <patrick.barringer@gmail.com>
2023-04-28 20:17:00 +0000

    Initial drag and drop play

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const list = [
+    { key: 0, name: "red" },
+    { key: 1, name: "green" },
+    { key: 2, name: "blue" },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <ul>
+        {list.map((listItem) => (
+          <ul
+            draggable
+            onDragStart={() => console.log(listItem.name)}
+            onDragEnter={() => console.log(listItem.name + " enter")}
+            onDragLeave={() => console.log(listItem.name + " exit")}
+            key={listItem.key}
+          >
+            {listItem.name}
+          </ul>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
